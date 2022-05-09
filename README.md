cb526628b2916763476e3c9e76825e07ae3b93ee
FaheemQuazi <FaheemQuazi@users.noreply.github.com>
2022-05-09 16:59:44 -0500

    Final Project Submission

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # SegmentRunner
 ECE 5440 Final Class Project - Gravity Guy on 7-segment displays
 
-## Team
+## Team (Gravity Guyz)
 
-- Faheem Quazi
-- Gabriel Torres
-- Zain Bhatti
-- Arash Shariatzadeh
+![Our Team Picture from MSTeams](teamPicture_GravityGuyz.png)
+- Faheem Quazi : Team Lead / Gameplay
+- Gabriel Torres : Display Output / Decoder
+- Zain Bhatti : Scoring / Score Tracking
+- Arash Shariatzadeh : Authentication / Log In
 
 ## Description
 
 We developed a version of Gravity Guy for the DE0-CV FPGA Development Kit. It utilizes the 7 segment displays as a game screen,
 where the bottom segments are the "floor" and top segments are the "ceiling". The player is a single vertical segment.
 with a button press, the player can move between the floor and ceiling segments, and the goal is to stay wherever there is a floor or ceiling.
+
+## Documentation and User Guide
+
+Check out the [doc/](doc/) folder for both the design manual and user guide.
--- sim/decoder/testbench_Decoder.v
+++ sim/decoder/testbench_Decoder.v
@@ -1,19 +1,42 @@
 `timescale 1 ns/100 ps
 module testbench_Decoder();
-	reg playerPos, showScore;
+	reg playerPos, showScore, loggedIn, clk;
 	reg [5:0] ceilingBits, floorBits;
-	reg [7:0] score;
+	reg [13:0] score;
 	wire [6:0] disp5, disp4, disp3, disp2, disp1, disp0;
 
-	Decoder DUT(ceilingBits, floorBits, playerPos, score, showScore, disp5, disp4, disp3, disp2, disp1, disp0);
+	always 
+		begin
+		clk = 0;
+		#10;
+		clk = 1;
+		#10;
+		end
+
+	Decoder DUT(clk, loggedIn, ceilingBits, floorBits, playerPos, score, showScore, disp5, disp4, disp3, disp2, disp1, disp0);
 
 	initial 
 		begin
-		showScore = 1'b1; score = 8'b11111111; ceilingBits = 6'b111111; floorBits = 6'b000000; playerPos = 1'b0;
-		#10 showScore = 1'b0; 
-		#10 showScore = 1'b1;
-		#10 score = 8'b01101111;
-		#10 showScore = 1'b0;
-		#10 showScore = 1'b1;
+		showScore = 1'b0; score = 14'b10011100001111; ceilingBits = 6'b111111; floorBits = 6'b000000; playerPos = 1'b0; loggedIn = 1'b0;
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		loggedIn = 1'b1;
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		showScore = 1'b1;
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		showScore = 1'b0;
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		showScore = 1'b1;
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		showScore = 1'b1;
 		end
 endmodule
